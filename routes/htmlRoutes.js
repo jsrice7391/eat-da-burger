87280e29562cd3378135f8bfe7d965483a40ef51
Justin Rice <jsrice7391@gmail.com>
2018-01-06 19:05:32 -0500

    Going to use JQuery for the pulls from the API which now works

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,16 @@
 var path = require("path");
+const db = require("../config/connection.js")
 
 
 
 module.exports = function(app) {
     app.get("/", function(req, res) {
-        res.render("index");
+        db.query("SELECT * FROM burgers", function(err, results) {
+            if (err) throw err;
+
+            res.render("index", { burgers: results });
+
+        })
+
     })
 }
