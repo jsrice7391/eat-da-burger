41bb37ba71c1b8c4e5750fd6bf0680e4577045c4
Justin Rice <jsrice7391@gmail.com>
2018-01-15 17:25:40 -0500

    Heroku port issue

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-    host: "localhost",
+    host: process.env.HOST,
     user: process.env.DBUSER,
     password: process.env.DBPASS,
     database: "burgers_db",
