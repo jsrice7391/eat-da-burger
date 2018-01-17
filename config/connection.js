8520f3ee2a6fcf7beeacf80613f157ee9157fd1f
Justin Rice <jsrice7391@gmail.com>
2018-01-17 15:03:49 -0500

    Works now

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.DBUSER,
-    password: process.env.DBPASS,
-    database: "burgers_db",
-    use_env_variable: process.env.JAWSDB_URL
-});
-
-
-module.exports = db;
+var connection;
+if (process.env.JAWSDB_URL) {
+    //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: process.env.HOST,
+        user: process.env.DBUSER,
+        password: process.env.DBPASS,
+        database: "burgers_db",
+    });
+};
